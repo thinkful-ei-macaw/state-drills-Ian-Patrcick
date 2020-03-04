9876719e67d1bb5d1c83fa0fd1179d6d6e1ba1be
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-03-04 13:19:18 -0700

    finished bomb

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React from "react";
 
 class Bomb extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      count: 0
-    }
+      count: 1
+      //output: "tock"
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      console.log(this.state.count);
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
   render() {
-    return (
-      <div>
-        <p>output</p>
-      </div>
-    )
+    if (this.state.count >= 8) {
+      clearInterval(this.interval);
+      return (
+        <div>
+          <p>BOOM!</p>
+        </div>
+      );
+    } else if (this.state.count % 2 === 0) {
+      return (
+        <div>
+          <p>Tock</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Tick</p>
+        </div>
+      );
+    }
   }
 }
 
-export default Bomb
+export default Bomb;
--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -1,39 +1,32 @@
-import React from 'react'
+import React from "react";
 
 class HelloWorld extends React.Component {
-
   handleWorld = () => {
-    this.setState({ who: 'world' })
-  }
+    this.setState({ who: "world" });
+  };
   handleFriend = () => {
-    this.setState({ who: 'friend' })
-  }
+    this.setState({ who: "friend" });
+  };
   handleReact = () => {
-    this.setState({ who: 'react' })
-  }
+    this.setState({ who: "react" });
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      who: 'something'
-    }
+      who: "something"
+    };
   }
 
   render() {
     return (
       <div>
         <p>Hello, {this.state.who}</p>
-        <button onClick={this.handleWorld}>
-          world
-      </button >
-        <button onClick={this.handleFriend}>
-          friend
-      </button>
-        <button onClick={this.handleReact}>
-          react
-      </button>
+        <button onClick={this.handleWorld}>world</button>
+        <button onClick={this.handleFriend}>friend</button>
+        <button onClick={this.handleReact}>react</button>
       </div>
-    )
+    );
   }
 }
-export default HelloWorld
+export default HelloWorld;
